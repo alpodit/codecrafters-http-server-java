afb5a9f84a422e50631c4883a4e48a8858a1599d
alperen <furkanalperenarici@gmail.com>
2024-03-21 17:12:11 +0300

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+  private static final String CRLF = "\r\n";
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
@@ -14,11 +15,31 @@ public class Main {
     
     try {
       serverSocket = new ServerSocket(4221);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
+      while (true) {
+        serverSocket.setReuseAddress(true);
+        clientSocket =
+            serverSocket.accept(); // Wait for connection from client.
+        System.out.println("accepted new connection");
+        handleClient(clientSocket);
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+  private static void handleClient(final Socket clientSocket) {
+    try (var outputStream = clientSocket.getOutputStream()) {
+      outputStream.write(encodeHttpResponse("HTTP/1.1 200 OK"));
+      outputStream.flush();
+    } catch (IOException e) {
+      System.err.println("IOException in client handler: " + e.getMessage());
+
+    }
+  }
+  private static byte[] encodeHttpResponse(String value) {
+    if (value == null) {
+      return String.format("-1%s%s", CRLF, CRLF).getBytes();
+    }
+
+    return String.format("%s%s%s", value, CRLF, CRLF).getBytes();
   }
 }
